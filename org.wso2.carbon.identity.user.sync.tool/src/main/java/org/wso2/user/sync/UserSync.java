d00f99869c23d400ddeed03b22ced6181b73600f
CharinduThisara <thisaraimc@gmail.com>
2024-03-24 22:54:33 +0530

    Initial Commit

---
+++ org.wso2.carbon.identity.user.sync.tool/src/main/java/org/wso2/user/sync/UserSync.java
@@ -0,0 +1,15 @@
+package org.wso2.user.sync;
+
+/**
+ * Hello world!
+ *
+ */
+public class UserSync 
+{
+    public void syncUsers() {
+        while (true) {
+            
+            System.out.println("Syncing users...");
+        }
+    }
+}
