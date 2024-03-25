8ea03a9c129ab64705b69bfe448dbd64bce94cb6
CharinduThisara <thisaraimc@gmail.com>
2024-03-25 21:14:19 +0530

    Add SyncAgent2 files and remove UserSync class

--- SyncAgent2/Build.sh
+++ SyncAgent2/Build.sh
@@ -2,8 +2,8 @@ home_path='../wso2is-7.0.0'
 
 JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64 mvn clean install
 
-rm $home_path/repository/components/dropins/com.user.sync.tool-1.0.0.jar
-cp target/com.user.sync.tool-1.0.0.jar $home_path/repository/components/dropins/
+rm $home_path/repository/components/dropins/com.sync.tool-1.0-SNAPSHOT.jar
+cp target/com.sync.tool-1.0-SNAPSHOT.jar $home_path/repository/components/dropins/
 
 # rm $home_path/repository/conf/deployment.toml
 # cp ../deployment.toml $home_path/repository/conf/
---
+++ SyncAgent2/src/main/java/com/sync/tool/internal/ReadagentServiceComponent.java
@@ -0,0 +1,30 @@
+package com.sync.tool.internal;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.*;
+import org.wso2.carbon.user.core.service.RealmService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import com.sync.tool.Readagent;
+
+@Component(
+    name = "com.sync.tool",
+    immediate = true
+)
+public class ReadagentServiceComponent {
+
+    private static final Log log = LogFactory.getLog(Readagent.class);
+    private static RealmService realmService;
+
+    @Activate
+    protected void activate(ComponentContext context) {
+        BundleContext bundleContext = context.getBundleContext();
+        bundleContext.registerService(Readagent.class.getName(), new Readagent(), null);
+        log.info("Readagent bundle is activated");
+        log.info("-------------------------------------");
+        log.info("-------------------------------------");
+        Readagent.read();
+    }
+    
+}
